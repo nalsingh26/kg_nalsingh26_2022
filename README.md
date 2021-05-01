f669550a72ad5d4e9e0f4daa58bcabe66d2979a8
Nalini Singh <nalinisingh@umass.edu>
2021-05-01 12:26:30 -0400

    Adding documentation and README files, refactoring a part of the code
    Added inline comments and README.md file. Moves charNum inside the main method.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kg_nalsingh26_2022
+This repository was created for homework assignment of a company.
+### Requirements:
+Golang should be installed in the system. If it's not, follow <a href="https://golang.org/doc/install" target="_blank">these</a> steps.
+### Execution command format:
+go run main.go [list of numbers]
+### Example:
+####Cmd:s
+go run main.go 199 99 456 32
+
+####Expected output:
+OneNineNine,NineNine,FourFiveSix,ThreeTwo
--- main.go
+++ main.go
@@ -5,27 +5,32 @@ import (
   "os"
 )
 
-var charNum = map[rune]string{
-  48: "Zero",
-  49: "One",
-  50: "Two",
-  51: "Three",
-  52: "Four",
-  53: "Five",
-  54: "Six",
-  55: "Seven",
-  56: "Eight",
-  57: "Nine",
-}
-
 func main() {
+  // creating character to string map
+  var charNum = map[rune]string{
+    48: "Zero",
+    49: "One",
+    50: "Two",
+    51: "Three",
+    52: "Four",
+    53: "Five",
+    54: "Six",
+    55: "Seven",
+    56: "Eight",
+    57: "Nine",
+  }
+  // taking inline arguments
   nums_string := os.Args[1:]
+  // creating an empty result string
   result := ""
+  //iterating over the array of numbers
   for _, num := range nums_string {
+    //iterating over digits of a number
     for _, char := range num {
       result += charNum[char]
     }
     result += ","
   }
+  // printing the output
   fmt.Println(result[:len(result)-1])
 }
